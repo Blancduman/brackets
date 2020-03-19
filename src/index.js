2c5a73d48e017a9eb34c6e2195d824ce8400cf8a
Ibragim Abbasov <blancduman@gmail.com>
2020-03-19 23:02:27 +0400

    feat: task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        const tmp = bracketsConfig[i].join("");
+        if (str.includes(tmp)) {
+            str = str.replace(tmp, "");
+            i = -1;
+        }
+    }
+
+    return !str;
+};
